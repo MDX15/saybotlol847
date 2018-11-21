1bcda4f780bd96d1276bc0e9629b062ee47251ee
MDX15 <45093539+MDX15@users.noreply.github.com>
2018-11-21 22:38:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,21 @@
+var prefix = "$";
     client.on('message', message => {
-        var prefix = "-";
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-     
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-     
-     
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-       
-       
-      });
-client.login(process.env.BOT_TOKEN); 
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "spam") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|`ADMINISTRATOR');
+ 
+ 
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+
+  }
+ });
+
+client.login(process.env.BOT_TOKEN);
