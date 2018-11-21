9cc2c774dfab54974513731e12664eab346005ca
MDX15 <45093539+MDX15@users.noreply.github.com>
2018-11-21 22:47:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,10 @@
-var prefix = "$";
-    client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
- 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
- 
-  let args = message.content.split(" ").slice(1);
- 
-  if (command == "spam") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|`ADMINISTRATOR');
- 
- 
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-
-  }
- });
+client.on('message',function(message) {
+	let prefix = "m#";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
 
 client.login(process.env.BOT_TOKEN);
